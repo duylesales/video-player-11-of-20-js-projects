8cd495e554cb77c723ddec12f85b7919aef5c287
Duy Node <duynode@Duys-MacBook-Pro.local>
2023-10-16 18:07:00 +0700

    seek on progressing bar

--- script.js
+++ script.js
@@ -41,6 +41,13 @@ function updateProgress() {
   currentTime.textContent = `${displayTime(video.currentTime)}/`;
   duration.textContent = `${displayTime(video.duration)}`;
 }
+// Click to seek within the video
+function setProgress(e) {
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+  console.log(newTime);
+}
 // Volume Controls --------------------------- //
 
 // Change Playback Speed -------------------- //
@@ -52,3 +59,4 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
